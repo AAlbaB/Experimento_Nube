c712d342c7115355074358d8f83d641ee127aa54
Andres Alba <o.alba@uniandes.edu.co>
2023-02-14 09:37:19 -0500

    Prueba 13

--- trayectos/src/__init__.py
+++ trayectos/src/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
+import os
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 def create_app(config_name):
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///trayectos.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['JWT_SECRET_KEY'] = 'frase-secreta'
     app.config['PROPAGATE_EXCEPTIONS'] = True
